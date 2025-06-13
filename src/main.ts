f34ae600124da8cdd120abc629bb1491d89da3d7
Matheus Lucas Batista <matheusifro2020@gmail.com>
2025-06-12 21:42:21 -0400

    implementing type of zod schema

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import './style.css';
+import { z } from 'zod';
 import { EnrollmentSchema } from './validators/enrollmentSchema';
+
+type Enrollment = z.infer<typeof EnrollmentSchema>;
 
 const form = document.querySelector<HTMLFormElement>('#form')!;
 const name = document.querySelector<HTMLInputElement>('#name')!;
@@ -10,43 +13,35 @@ const course = document.querySelector<HTMLSelectElement>('#course')!;
 const description = document.querySelector<HTMLTextAreaElement>('#description')!;
 const terms = document.querySelector<HTMLInputElement>('#terms')!;
 
-interface Enrollment {
-    name: string,
-    email: string,
-    gender: string | undefined,
-    course: string, 
-    description: string,
-    terms: boolean
-}
-
 function limparFormulario() {
-    form.reset()
+    form.reset();
 }
 
 function getGenderSelected() {
-    if(maleGender.checked) return 'male';
-    if(femaleGender.checked) return 'female';
+    if (maleGender.checked) return 'male';
+    if (femaleGender.checked) return 'female';
+    return '';
 }
 
-export let enrollments: Enrollment[] = [];
+let enrollments: Enrollment[] = [];
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const e: Enrollment = {
-        name: name.value,
-        email: email.value,
-        gender: getGenderSelected(),
-        course: course.value,
-        description: description.value,
-        terms: terms.checked
+    try {
+        const parsedData = EnrollmentSchema.parse({
+            name: name.value,
+            email: email.value,
+            gender: getGenderSelected(),
+            course: course.value,
+            description: description.value,
+            terms: terms.checked
+        });
+
+        enrollments.push(parsedData);
+        console.log(JSON.stringify(enrollments));
+        limparFormulario();
+    } catch (err) {
+        console.error("Erro de validação:", err);
     }
-
-    const parsedData = EnrollmentSchema.parse(e);
-
-    enrollments.push(e);
-
-    console.log(JSON.stringify(enrollments))
-
-    limparFormulario()
-})
+});
