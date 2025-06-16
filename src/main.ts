f2c5e02b929df9defee56bb5fe568d6d9e7f6f3a
Matheus Batista <matheusifro2020@gmail.com>
2025-06-16 12:04:16 -0400

    adjusting icon according to the mode chosen

--- src/main.ts
+++ src/main.ts
@@ -45,9 +45,18 @@ form.addEventListener('submit', async(event) => {
     }
 });
 
-const changeMode = document.getElementById("change-mode");
+const changeMode = document.getElementById("change-mode") as HTMLImageElement;
 const body = document.body;
 
 changeMode?.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
+
+  if (body.classList.contains('dark-mode')) {
+    changeMode.src = '/img/light-mode-icon.png';
+    changeMode.alt = 'Simbolo de modo claro';
+    
+  } else {
+    changeMode.src = '/img/dark-mode-icon.png';
+  changeMode.alt = 'Simbolo de modo escuro';
+  }
 });
