f86ef2d7f9be0a0773b937bc3994cbc8bf8fd985
Matheus Batista <matheusifro2020@gmail.com>
2025-06-22 15:23:42 -0400

    implementing the manipulation of the data.json file into the data page

--- src/data.ts
+++ src/data.ts
@@ -2,3 +2,39 @@ import './style.css';
 import { darkMode } from './darkMode.ts';
 
 darkMode();
+
+const container = document.getElementById('data-list')
+
+async function loadData() {
+    try {
+        const response = await fetch('http://localhost:3000/data');
+        const enrolled = await response.json();
+
+        if(Array.isArray(enrolled)) {
+            enrolled.forEach((person) => {
+                function description() {
+                    if(person.description === ""){
+                        return "Nenhuma descrição fornecida"
+                    } else {
+                        return person.description
+                    }
+                }
+
+                const div = document.createElement('div');
+                div.className = 'data';
+                div.innerHTML = `
+                    <div>Nome: <span>${person.name}</span> </div>
+                    <div>E-mail: <span>${person.email}</span></div>
+                    <div>Sexo: <span>${person.gender}</span></div>
+                    <div>Curso: <span>${person.course}</span></div>
+                    <div>Descrição: <span>${description()}</span></div>
+                `;
+                container?.appendChild(div);
+            })
+        }
+    }catch (error){
+        console.log('Erro ao carregar dados:', error)
+    }
+}
+
+loadData()
